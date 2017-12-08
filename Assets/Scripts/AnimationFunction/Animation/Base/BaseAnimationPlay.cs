9f48a94ee5e97583cbe33f47e79d3a5aaca9fc4f
MyBlog <1070663764@qq.com>
2017-12-08 15:18:08 +0800

    状态机接口

--- Assets/Scripts/AnimationFunction/Animation/Base/BaseAnimationPlay.cs
+++ Assets/Scripts/AnimationFunction/Animation/Base/BaseAnimationPlay.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public abstract class BaseAnimationPlay
+public abstract class BaseAnimationPlay : BaseFSM
 {
     //处理混合指令，实际上只能有一个指令起作用，当前版本混合指令的处理只是筛选   高级的用法其实应该是混合动画。
     //某些情况实际上很难触发，安全处理
