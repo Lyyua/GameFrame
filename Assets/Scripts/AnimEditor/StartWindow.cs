b8c9f44d2b8166403fad935d90a0eee530973dbc
MyBlog <1070663764@qq.com>
2017-12-29 16:20:07 +0800

    开始入口

---
+++ Assets/Scripts/AnimEditor/StartWindow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartWindow : MonoBehaviour
+{
+    void Start()
+    {
+        UIWindowMgr.Instance.PushPanel<UIAnimOPChoose>();
+    }
+}
