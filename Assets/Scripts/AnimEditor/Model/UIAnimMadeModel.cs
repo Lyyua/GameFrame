6389732286a33db80fdaa2c86c744352140b9f10
MyBlog <1070663764@qq.com>
2017-12-29 10:52:16 +0800

    动画编辑功能的Model类

---
+++ Assets/Scripts/AnimEditor/Model/UIAnimMadeModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class UIAnimMadeModelConst
+{
+    public const string KEY_AnimInfo = "AnimInfo";
+}
+
+public class UIAnimMadeModel : UIBaseModel
+{
+    private Animation curAnim;
+    public Animation CurAnim
+    {
+        get
+        {
+            return curAnim;
+        }
+
+        set
+        {
+            Animation old = curAnim;
+            curAnim = value;
+            ValueChangeArgs ve = new ValueChangeArgs(UIAnimMadeModelConst.KEY_AnimInfo, old, value);
+            DispatchValueUpdateEvent(ve);
+        }
+    }
+}
+
