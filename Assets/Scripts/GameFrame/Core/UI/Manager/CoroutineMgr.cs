4e61b36e85deb689978a80fb01d0e045ec718a3a
MyBlog <1070663764@qq.com>
2017-12-29 14:36:21 +0800

    协程返回和停止

--- Assets/Scripts/GameFrame/Core/UI/Manager/CoroutineMgr.cs
+++ Assets/Scripts/GameFrame/Core/UI/Manager/CoroutineMgr.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class CoroutineMgr : Singleton<CoroutineMgr>
 {
     public void StartCoroutine(IEnumerator cor)
     {
         ApplicationMgr.Instance.StartCoroutine(cor);
+    }
+
+    public Coroutine StartCoroutineReturn(IEnumerator cor)
+    {
+        return ApplicationMgr.Instance.StartCoroutine(cor);
     }
 
     public void StartCoroutine(string cor)
@@ -15,6 +21,11 @@ public class CoroutineMgr : Singleton<CoroutineMgr>
 
     // 停止协程
     public void StopCoroutine(IEnumerator cor)
+    {
+        ApplicationMgr.Instance.StopCoroutine(cor);
+    }
+
+    public void StopCoroutine(Coroutine cor)
     {
         ApplicationMgr.Instance.StopCoroutine(cor);
     }
