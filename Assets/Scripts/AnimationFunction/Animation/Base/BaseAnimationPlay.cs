114210b3eee9132014e2f096d80fb2ab2376083b
MyBlog <1070663764@qq.com>
2018-01-19 16:24:29 +0800

    动画状态基类

--- Assets/Scripts/AnimationFunction/Animation/Base/BaseAnimationPlay.cs
+++ Assets/Scripts/AnimationFunction/Animation/Base/BaseAnimationPlay.cs
@@ -6,8 +6,8 @@ public abstract class BaseAnimationPlay : BaseFSM
 {
     //处理混合指令，实际上只能有一个指令起作用，当前版本混合指令的处理只是筛选   高级的用法其实应该是混合动画。
     //某些情况实际上很难触发，安全处理
+    public abstract void HandleInput(AnimationCMD cmd);
     public abstract AnimationCMD CMDFilter(List<AnimationCMD> cmds);
-    public virtual void HandleInput(AnimationCMD cmd) { }
-    public abstract void OnUpdate();
-    public abstract void OnExit();
+    public virtual void OnUpdate() { }
+    public virtual void OnExit() { }
 }
